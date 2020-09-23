c74fe8569554936a8a33d4072b75c5befd37d444
ZhangZhiHua0927 <>
2020-09-23 17:49:04 +0800

    upload

---
+++ 展开预览/展开预览/model.swift
@@ -0,0 +1,19 @@
+//
+//  model.swift
+//  展开预览
+//
+//  Created by foxdingding on 2018/12/10.
+//  Copyright © 2018年 foxdingding. All rights reserved.
+//
+
+import UIKit
+
+class model: NSObject {
+
+    var rowHeight : CGFloat = 44
+}
+
+class model1: NSObject {
+
+    var rowHeight : CGFloat = 44
+}
